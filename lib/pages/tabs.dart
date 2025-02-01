bd220d296a7e942a0a31d7859062e391c3d14afe
Adityarayyy <adityasray46@gmail.com>
2025-02-02 04:26:56 +0530

    added bottom navbar

---
+++ lib/pages/tabs.dart
@@ -0,0 +1,50 @@
+import 'package:appdev/create_trip.dart';
+import 'package:appdev/translate.dart';
+import 'package:appdev/trip.dart';
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 1;
+
+  void selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = Trip();
+
+    if (_selectedPageIndex == 1) {
+      activePage = TranslatePage();
+    } else if (_selectedPageIndex == 2) {
+      activePage = CreateTrip();
+    }
+
+    return Scaffold(
+      // appBar: AppBar(),
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          onTap: (int index) {
+            selectPage(index);
+          },
+          currentIndex: _selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore), label: 'H'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.translate_outlined), label: 'j'),
+            BottomNavigationBarItem(icon: Icon(Icons.create), label: '9'),
+          ]),
+    );
+  }
+}
---
+++ lib/pages/translate.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TranslatePage extends StatefulWidget {
+  const TranslatePage({super.key});
+
+  @override
+  State<TranslatePage> createState() => _TranslatePageState();
+}
+
+class _TranslatePageState extends State<TranslatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Enter translate language")),
+    );
+  }
+}
